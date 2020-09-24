ce5b154cf579542ef4c09787392d8171ce419a73
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-09-24 11:42:21 +0200

    commit

--- src/main/java/dev/paie/controller/CollegueCtrl.java
+++ src/main/java/dev/paie/controller/CollegueCtrl.java
@@ -1,4 +1,5 @@
 package dev.paie.controller;
+
 import java.util.List;
 
 import javax.validation.Valid;
@@ -15,35 +16,29 @@ import dev.paie.dto.CreerCollegueResponseDto;
 import dev.paie.entity.Collegue;
 import dev.paie.service.CollegueService;
 
-
 @RestController
 @RequestMapping("collegues")
 public class CollegueCtrl {
-	
-	
+
 	private CollegueService collegueService;
-	
-	
+
 	public CollegueCtrl(CollegueService collegueService) {
 		super();
 		this.collegueService = collegueService;
 	}
-
-
 
 	// GET /collegues
 	@GetMapping
 	public List<String> rechercherMatriculesParNom(@RequestParam String nom) {
 		return this.collegueService.rechercherParNom(nom);
 	}
-	
+
 	// POST /collegues
 	@PostMapping
 	public CreerCollegueResponseDto creerNouveauCollegue(@RequestBody @Valid CreerCollegueRequestDto dto) {
 		Collegue collegueCree = this.collegueService.creerCollegue(dto);
-		
+
 		return new CreerCollegueResponseDto(collegueCree.getMatricule(), collegueCree.getEmail());
 	}
-	
 
 }
