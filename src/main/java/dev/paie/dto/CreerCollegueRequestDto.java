59b754a1eb93034186d2a53bd6e9087470ddab96
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-09-28 11:54:56 +0200

    Commit création collegue API

--- src/main/java/dev/paie/dto/CreerCollegueRequestDto.java
+++ src/main/java/dev/paie/dto/CreerCollegueRequestDto.java
@@ -25,34 +25,48 @@ public class CreerCollegueRequestDto {
 	@Past
 	private LocalDate dateDeNaissance;
 	@NotBlank
+	private String email;
+	@NotBlank
 	private String photoUrl;
-	
-	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public String getPrenoms() {
 		return prenoms;
 	}
+
 	public void setPrenoms(String prenoms) {
 		this.prenoms = prenoms;
 	}
+
 	public LocalDate getDateDeNaissance() {
 		return dateDeNaissance;
 	}
+
 	public void setDateDeNaissance(LocalDate dateDeNaissance) {
 		this.dateDeNaissance = dateDeNaissance;
 	}
+
 	public String getPhotoUrl() {
 		return photoUrl;
 	}
+
 	public void setPhotoUrl(String photoUrl) {
 		this.photoUrl = photoUrl;
 	}
-	
-	
-}
 
+}
